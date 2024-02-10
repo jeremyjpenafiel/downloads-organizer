bfbc0a72b00e8e78fa21b99992afbd3260e51179
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2024-02-11 01:04:49 +0800

    progress

--- script.py
+++ script.py
@@ -1,11 +1,46 @@
 import os
+from pathlib import Path
 
 
-# path: str = f"{os.curdir}/Downloads"
-#
-# try:
-#     os.mkdir(path)
-# except OSError as error:
-#     print(error)
+DOWNLOADS = Path.home() / 'Downloads'
+FOLDER_NAMES = ["PDFs", "Images", "Videos", "Word Documents"]
+FOLDER_DICT = {
+    ".pdf": "PDFs",
+    ".jpg": "JPG",
+    ".png": "Images",
+    ".jpeg": "Images",
+    ".gif": "Images",
+    ".bmp": "Images",
+    ".tiff": "Images",
+    "mp4": "Videos",
+    ".mov": "Videos",
+    ".mpg": "Videos",
+    ".avi": "Videos",
+    "doc": "Word Documents",
+    ".docx": "Word Documents"}
+
+
+# TODO: Check if directory of current script is Downloads
+# if os.curdir != DOWNLOADS:
+#     raise Exception("Move this file to your Downloads folder")
+
+# TODO: Check if folders to be made already exists
+
+for folder in FOLDER_DICT.values():
+    path: str = f"{os.curdir}/{folder}"
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+# TODO: Move all files to corresponding folders
+for file in os.scandir():
+    if not file.is_file():
+        continue
+        
+    for file_type in FOLDER_DICT.keys():
+        if file.name.endswith(file_type):
+            os.rename(file.path, file.path + FOLDER_DICT[file_type])
+
+# TODO: Schedule every day?
+
 
 
